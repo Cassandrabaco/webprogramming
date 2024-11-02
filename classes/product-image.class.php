aaef221cb3a2646dd1550bec2aea234d3098d9f9
Cassandra <143484957+Cassandrabaco@users.noreply.github.com>
2024-11-02 18:08:34 +0800

    Update product-image.class.php
    Restricting the upload to max 5mb

--- classes/product-image.class.php
+++ classes/product-image.class.php
@@ -7,14 +7,32 @@ class ProductImage extends Product{
     public $image_role = '';
 
     function addImage() {
-        $sql = "INSERT INTO product_image (product_id, file_path, image_role) VALUES (:product_id, :file_path, :image_role);";
-        $query = $this->db->connect()->prepare($sql);
+        $maxFileSize = 5 * 1024 * 1024;
 
-        $last_inserted_product = $this->db->connect()->lastInsertId();
+        if (isset($_FILES['uploaded_file'])) 
+        { $fileSize = $_FILES['uploaded_file']['size'];
 
-        $query->bindParam(':product_id', $last_inserted_product);
-        $query->bindParam(':file_path', $this->file_path);
-        $query->bindParam(':image_role', $this->image_role);
-        return $query->execute();
-    }
+         if ($fileSize > $maxFileSize) { 
+             echo "Error: File size exceeds the maximum limit of 5MB."; 
+             return false;
+         } else {
+             $this->file_path = "uploads/" . $_FILES['uploaded_file']['name']; 
+             move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $this->file_path);
+             
+             $sql = "INSERT INTO product_image (product_id, file_path, image_role) VALUES (:product_id, :file_path, :image_role);";
+             $query = $this->db->connect()->prepare($sql);
+             
+             $last_inserted_product = $this->db->connect()->lastInsertId();
+             
+             $query->bindParam(':product_id', $last_inserted_product); 
+             $query->bindParam(':file_path', $this->file_path); 
+             $query->bindParam(':image_role', $this->image_role);
+
+             return $query->execute(); 
+         } 
+        } else { 
+            echo "No file was uploaded."; 
+            return false; 
+        } 
+    } 
 }
